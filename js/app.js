260b039efdbe20bf1bfc1385ee5ce14368b77be3
jason606 <stonegee606@yahoo.com>
2022-07-09 10:27:40 +0000

    Carousel

--- js/app.js
+++ js/app.js
@@ -1,7 +1,25 @@
-const btnMenu = document.querySelector('.trigger'),
-    menuClass = document.querySelector('nav')
+const btnMenu = document.querySelector(".trigger"),
+  menuClass = document.querySelector("nav");
 
-btnMenu.addEventListener('click', ()=>{
-    menuClass.classList.toggle('showmenu')
-})
+btnMenu.addEventListener("click", () => {
+  menuClass.classList.toggle("showmenu");
+});
 
+const btnLight = document.querySelector(".t-light"),
+  addClass = document.querySelector(".site");
+btnLight.addEventListener("click", () => {
+  addClass.classList.toggle("turnon");
+});
+
+const swiper = new Swiper(".swiper", {
+  slidesPerView: 1,
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+    cliackable: true,
+  },
+  breakpoints: {
+    641: { slidesPerView: 2 },
+    992: { slidesPerView: 3 },
+  },
+});
